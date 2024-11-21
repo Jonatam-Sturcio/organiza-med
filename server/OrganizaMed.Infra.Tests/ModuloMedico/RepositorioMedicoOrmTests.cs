b1ddc09784b3d8341f555a7bb1a7804ca8dfa6c8
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-21 15:36:39 -0300

    Criação do projeto de teste de integração

---
+++ server/OrganizaMed.Infra.Tests/ModuloMedico/RepositorioMedicoOrmTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OrganizaMed.Dominio.Compartilhado;
+using OrganizaMed.Dominio.ModuloMedico;
+using OrganizaMed.Infra.Orm.Compartilhado;
+
+namespace OrganizaMed.Infra.Tests.ModuloMedico;
+
+[TestClass]
+public class RepositorioMedicoOrmTests
+{
+	private readonly OrganizaMedDbContext db;
+
+	public RepositorioMedicoOrmTests()
+	{
+		var builder = new DbContextOptionsBuilder<OrganizaMedDbContext>();
+		IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<RepositorioMedicoOrmTests>().Build();
+
+		builder.UseSqlServer(config["SQL_CONNECTION_STRING"]);
+
+		db = new OrganizaMedDbContext(builder.Options);
+
+		db.Set<Medico>().RemoveRange(db.Set<Medico>());
+		db.Set<AtividadeBase>().RemoveRange(db.Set<AtividadeBase>());
+
+		db.SaveChanges();
+	}
+}
