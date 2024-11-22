125c5ec118abcc767837467cf803d9114d4be5bd
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-22 14:35:52 -0300

    Testes de Integração de Médico + classe tests base

---
+++ server/OrganizaMed.Infra.Tests/Compartilhado/RepositorioEmOrmTestsBase.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OrganizaMed.Dominio.Compartilhado;
+using OrganizaMed.Dominio.ModuloMedico;
+using OrganizaMed.Infra.Orm.Compartilhado;
+using OrganizaMed.Infra.Orm.ModuloAtividades;
+using OrganizaMed.Infra.Orm.ModuloMedico;
+using OrganizaMed.Infra.Tests.ModuloMedico;
+using System.Diagnostics;
+
+namespace OrganizaMed.Infra.Tests.Compartilhado;
+
+public abstract class RepositorioEmOrmTestsBase
+{
+	protected OrganizaMedDbContext dbContext;
+	protected RepositorioMedicoOrm repositorioMedico;
+	protected RepositorioAtividadeOrm repositorioAtividade;
+
+	[TestInitialize]
+	public void Inicializar()
+	{
+		var config = new ConfigurationBuilder().AddUserSecrets<RepositorioMedicoOrmTests>().Build();
+
+		var options = new DbContextOptionsBuilder<OrganizaMedDbContext>()
+			.UseSqlServer(config["SQLSERVER_CONNECTION_STRING"])
+			.Options;
+
+		dbContext = new OrganizaMedDbContext(options);
+
+		Debug.WriteLine("Criando banco de dados");
+		dbContext.Database.EnsureDeleted();
+
+		dbContext.Database.Migrate();
+		Debug.WriteLine("Banco de dados criado com sucesso");
+
+		dbContext.Set<Medico>().RemoveRange(dbContext.Set<Medico>());
+		dbContext.Set<AtividadeBase>().RemoveRange(dbContext.Set<AtividadeBase>());
+
+		repositorioMedico = new(dbContext);
+		repositorioAtividade = new(dbContext);
+	}
+}
--- server/OrganizaMed.Infra.Tests/ModuloMedico/RepositorioMedicoOrmTests.cs
+++ server/OrganizaMed.Infra.Tests/ModuloMedico/RepositorioMedicoOrmTests.cs
@@ -1,52 +1,79 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using OrganizaMed.Dominio.ModuloMedico;
-using OrganizaMed.Infra.Orm.Compartilhado;
-using OrganizaMed.Infra.Orm.ModuloMedico;
-using System.Diagnostics;
+﻿using OrganizaMed.Dominio.ModuloMedico;
+using OrganizaMed.Infra.Tests.Compartilhado;
 
 namespace OrganizaMed.Infra.Tests.ModuloMedico;
 
 [TestClass]
-public class RepositorioMedicoOrmTests
+public class RepositorioMedicoOrmTests : RepositorioEmOrmTestsBase
 {
-	private readonly OrganizaMedDbContext db;
-
-	public RepositorioMedicoOrmTests()
-	{
-		var config = new ConfigurationBuilder().AddUserSecrets<RepositorioMedicoOrmTests>().Build();
-
-		var options = new DbContextOptionsBuilder<OrganizaMedDbContext>()
-			.UseSqlServer(config["SQLSERVER_CONNECTION_STRING"])
-			.Options;
-
-		db = new OrganizaMedDbContext(options);
-	}
-
-	[TestInitialize]
-	public void Initialize()
-	{
-		Debug.WriteLine("Criando banco de dados");
-		db.Database.EnsureDeleted();
-
-		db.Database.Migrate();
-		Debug.WriteLine("Banco de dados criado com sucesso");
-	}
-
 	[TestMethod]
-	public void Deve_inserir_medico()
+	public void Deve_inserir_medico_com_sucesso()
 	{
 		//arrange
 		Medico registro = new Medico("Cleiton", "cardio", "12345-cd");
 
-		var repositorio = new RepositorioMedicoOrm(db);
-
 		//action
-		repositorio.Inserir(registro);
+		repositorioMedico.Inserir(registro);
 
 		//assert
-		Medico medicoEncontrado = repositorio.SelecionarPorId(registro.Id);
+		Medico medicoEncontrado = repositorioMedico.SelecionarPorId(registro.Id);
 
-		Assert.AreEqual("Cleiton", registro.Nome);
+		Assert.AreEqual("Cleiton", medicoEncontrado.Nome);
+	}
+
+	[TestMethod]
+	public void Deve_editar_medico_com_sucesso()
+	{
+		//arrange
+		Medico registro = new Medico("Cleiton", "cardio", "12345-cd");
+		repositorioMedico.Inserir(registro);
+
+		Medico registroAtualizado = repositorioMedico.SelecionarPorId(registro.Id);
+		registroAtualizado.Nome = "Cleiton Editado";
+
+		//action
+		repositorioMedico.Editar(registroAtualizado);
+
+		//assert
+		Medico medicoEncontrado = repositorioMedico.SelecionarPorId(registro.Id);
+
+		Assert.AreEqual("Cleiton Editado", medicoEncontrado.Nome);
+	}
+
+	[TestMethod]
+	public void Deve_excluir_medico_com_sucesso()
+	{
+		//arrange
+		Medico registro = new Medico("Cleiton", "cardio", "12345-cd");
+		repositorioMedico.Inserir(registro);
+
+		Medico registroExcluido = repositorioMedico.SelecionarPorId(registro.Id);
+
+		//action
+		repositorioMedico.Excluir(registroExcluido);
+
+		//assert
+		Medico medicoEncontrado = repositorioMedico.SelecionarPorId(registroExcluido.Id);
+
+		Assert.IsNull(medicoEncontrado);
+	}
+
+	[TestMethod]
+	public void Deve_selecionar_todos_medicos_com_sucesso()
+	{
+		//arrange
+		Medico registro = new Medico("Cleiton", "cardio", "12345-cd");
+		Medico registro2 = new Medico("Cleiton2", "cardio", "12345-cd");
+		Medico registro3 = new Medico("Cleiton3", "cardio", "12345-cd");
+
+		//action
+		repositorioMedico.Inserir(registro);
+		repositorioMedico.Inserir(registro2);
+		repositorioMedico.Inserir(registro3);
+
+		//assert
+		var medicosEncontrado = repositorioMedico.SelecionarTodos();
+
+		Assert.AreEqual(3, medicosEncontrado.Count);
 	}
 }
