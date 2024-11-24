07d58365cc915426788c70080ea43c90a2b11b1a
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-24 13:46:41 -0300

    Correção de bug de validação de CRM já cadastrada

--- server/OrganizaMed.Dominio/ModuloMedico/ValidadorMedico.cs
+++ server/OrganizaMed.Dominio/ModuloMedico/ValidadorMedico.cs
@@ -18,8 +18,8 @@ public class ValidadorMedico : AbstractValidator<Medico>
 		repositorioMedico = repoMedico;
 
 		RuleFor(x => x.Nome).NotEmpty().WithMessage("O nome é obrigatório");
-		RuleFor(x => x.Especialidade).NotEmpty().WithMessage("A especialidade é obrigatória")
+		RuleFor(x => x.Especialidade).NotEmpty().WithMessage("A especialidade é obrigatória");
+		RuleFor(x => x.CRM).NotEmpty().Matches(@"^\d{5}-[A-Z]{2}$")
 			.MustAsync(async (crm, cancel) => !await Task.Run(() => repositorioMedico.CrmExiste(crm))).WithMessage("Este CRM já está registrado.");
-		RuleFor(x => x.CRM).NotEmpty().Matches(@"^\d{5}-[A-Z]{2}$");
 	}
 }
