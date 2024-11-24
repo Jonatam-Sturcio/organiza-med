a85f81f90eeae8479433496479a60f8eee771a46
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-24 13:56:56 -0300

    Validador de Atividade

---
+++ server/OrganizaMed.Dominio/Entidades/ValidadorAtividade.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OrganizaMed.Dominio.Compartilhado;
+
+namespace OrganizaMed.Dominio.Entidades;
+
+public class ValidadorAtividade : AbstractValidator<AtividadeBase>
+{
+	public ValidadorAtividade()
+	{
+		RuleFor(x => x).Must(x =>
+		{
+			var erros = x.ValidarHorario();
+			return !erros.Any();
+		})
+		.WithMessage("Horário indisponível.");
+	}
+}
