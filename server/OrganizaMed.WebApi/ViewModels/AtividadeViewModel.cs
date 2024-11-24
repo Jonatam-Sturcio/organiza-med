17756cd2f734ab710b7919b276df22cace969848
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-24 14:04:58 -0300

    ViewModel Atividade

---
+++ server/OrganizaMed.WebApi/ViewModels/AtividadeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using OrganizaMed.Dominio.Compartilhado;
+using OrganizaMed.Dominio.ModuloMedico;
+
+namespace OrganizaMed.WebApi.ViewModels;
+
+public class InserirAtividadeViewModel
+{
+	public DateTime HoraInicio { get; set; }
+	public DateTime HoraTermino { get; set; }
+	public List<Medico> Medicos { get; set; }
+	public TipoAtividadeEnum TipoAtividade { get; set; }
+}
+
+public class EditarAtividadeViewModel
+{
+	public DateTime HoraInicio { get; set; }
+	public DateTime HoraTermino { get; set; }
+	public List<Medico> Medicos { get; set; }
+	public TipoAtividadeEnum TipoAtividade { get; set; }
+}
+
+public class ListarAtividadeViewModel
+{
+	public Guid Id { get; set; }
+
+	public DateTime HoraInicio { get; set; }
+	public DateTime HoraTermino { get; set; }
+	public List<Medico> Medicos { get; set; }
+	public TipoAtividadeEnum TipoAtividade { get; set; }
+}
+
+public class VisualizarAtividadeViewModel
+{
+	public Guid Id { get; set; }
+
+	public DateTime HoraInicio { get; set; }
+	public DateTime HoraTermino { get; set; }
+	public List<Medico> Medicos { get; set; }
+	public TipoAtividadeEnum TipoAtividade { get; set; }
+}
