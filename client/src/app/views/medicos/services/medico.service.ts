63741714d0ef818521e15e6302ee5e8f938e166b
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-27 15:45:23 -0300

    Ajuste no mapping para todos os endpoints

--- client/src/app/views/medicos/services/medico.service.ts
+++ client/src/app/views/medicos/services/medico.service.ts
@@ -25,7 +25,9 @@ export class MedicoService {
   cadastrar(
     novoMedico: InserirMedicoViewModel
   ): Observable<MedicoInseridoViewModel> {
-    return this.http.post<MedicoInseridoViewModel>(this.url, novoMedico);
+    return this.http
+      .post<MedicoInseridoViewModel>(this.url, novoMedico)
+      .pipe(map((x) => this.processarRes(x) as MedicoInseridoViewModel));
   }
 
   editar(
@@ -34,32 +36,32 @@ export class MedicoService {
   ): Observable<MedicoEditadoViewModel> {
     const urlCompleto = `${this.url}/${id}`;
 
-    return this.http.put<MedicoEditadoViewModel>(urlCompleto, MedicoEditado);
+    return this.http
+      .put<MedicoEditadoViewModel>(urlCompleto, MedicoEditado)
+      .pipe(map((x) => this.processarRes(x) as MedicoEditadoViewModel));
   }
 
   excluir(id: string): Observable<MedicoExcluidoViewModel> {
     const urlCompleto = `${this.url}/${id}`;
 
-    return this.http.delete<MedicoExcluidoViewModel>(urlCompleto);
+    return this.http
+      .delete<MedicoExcluidoViewModel>(urlCompleto)
+      .pipe(map((x) => this.processarRes(x) as MedicoExcluidoViewModel));
   }
 
   selecionarTodos(): Observable<ListarMedicoViewModel[]> {
     const urlCompleto = `${this.url}?maisAtividades=false`;
-    return this.http.get<ListarMedicoViewModel[]>(urlCompleto).pipe(
-      map((x) => this.processarRes(x) as ListarMedicoViewModel[]),
-      tap((x) => console.log(x))
-    );
-  }
-
-  selecionarTodosOrdenado(): Observable<ListarMedicoViewModel[]> {
-    const urlCompleto = `${this.url}?maisAtividades=true`;
-    return this.http.get<ListarMedicoViewModel[]>(urlCompleto);
+    return this.http
+      .get<ListarMedicoViewModel[]>(urlCompleto)
+      .pipe(map((x) => this.processarRes(x) as ListarMedicoViewModel[]));
   }
 
   selecionarPorId(id: string): Observable<VisualizarMedicoViewModel> {
     const urlCompleto = `${this.url}/${id}`;
 
-    return this.http.get<VisualizarMedicoViewModel>(urlCompleto);
+    return this.http
+      .get<VisualizarMedicoViewModel>(urlCompleto)
+      .pipe(map((x) => this.processarRes(x) as VisualizarMedicoViewModel));
   }
 
   private processarRes(res: any): any {
