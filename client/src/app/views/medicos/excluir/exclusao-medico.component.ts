8cd4216ae308cdc685274992bbac9d56ea74b6af
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-12-01 15:02:03 -0300

    Ajuste em excluir para exibir atividades do médico

--- client/src/app/views/medicos/excluir/exclusao-medico.component.ts
+++ client/src/app/views/medicos/excluir/exclusao-medico.component.ts
@@ -12,11 +12,13 @@ import {
 import { MedicoService } from '../services/medico.service';
 import { MatCardModule } from '@angular/material/card';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { tipoAtividadeEnum } from '../../atividades/models/tipoAtividadeEnum';
 
 @Component({
   selector: 'app-exclusao-medico',
   standalone: true,
   imports: [
+    NgForOf,
     RouterLink,
     MatCardModule,
     MatIconModule,
@@ -26,7 +28,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   templateUrl: './exclusao-medico.component.html',
 })
 export class ExclusaoMedicoComponent implements OnInit {
-  Medico?: ListarMedicoViewModel;
+  Medico?: VisualizarMedicoViewModel;
 
   constructor(
     private route: ActivatedRoute,
@@ -53,5 +55,13 @@ export class ExclusaoMedicoComponent implements OnInit {
 
       this.router.navigate(['/medicos']);
     });
+  }
+  public obterTextoTipoAtividade(prioridade: tipoAtividadeEnum): string {
+    return tipoAtividadeEnum[Number(prioridade)];
+  }
+  public formatarData(data: Date): string | null {
+    var date = new Date(data);
+    var [ano, mes, dia] = date.toISOString().split('T')[0].split('-');
+    return `${dia}/${mes}/${ano}`;
   }
 }
