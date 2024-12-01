f2e89eb1a3a02a4b311101f66f84742359ecdc98
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-12-01 16:23:22 -0300

    Filtro por tipo de atividade

--- client/src/app/views/atividades/atividades.routes.ts
+++ client/src/app/views/atividades/atividades.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ListagemAtividadesComponent } from './listar/listagem-atividades.component';
 
-export const AtividadesRoutes: Routes = [];
+export const AtividadesRoutes: Routes = [
+  { path: '', redirectTo: 'listar', pathMatch: 'full' },
+  { path: 'listar', component: ListagemAtividadesComponent },
+];
